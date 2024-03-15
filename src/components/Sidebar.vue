0075774440ee0ef4d5d3c98be0aa175bcf2c2711
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-03-15 23:29:39 +0700

    add localStorage on toggleMenu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -44,10 +44,14 @@
 <script setup>
 import { ref } from 'vue'
 
-const is_expanded = ref(false);
+//const is_expanded = ref(false);
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true");
+
 
 const ToggleMenu = () => {
     is_expanded.value = !is_expanded.value
+
+    localStorage.setItem("is_expanded", is_expanded.value)
 }
 </script>
 
